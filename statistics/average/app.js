a6f4fb8051a1b20290d968b86c0d8edba8e7c14d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-26 21:57:54 -0500

    :sparkles: Average

---
+++ statistics/average/app.js
@@ -0,0 +1,11 @@
+const NUMBERS = document.getElementById('numbers');
+const ANSWER = document.getElementById('answer');
+
+const getAverage = (array) => array.reduce((a, b) => a + b, 0) / array.length;
+
+const strToArr = (str) => str.split(',');
+const propertyToInt = (array) => array.map(Number);
+
+const printAverage = () => {
+  ANSWER.textContent = getAverage(propertyToInt(strToArr(NUMBERS.value)));
+};
